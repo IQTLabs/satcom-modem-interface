87fb03954f89b94795f7e6c7006993dde5214640
Robert Caudill <rcaudill@iqt.org>
2021-12-15 16:34:51 -0500

    add sercom config and low power interrupt

--- satcom-modem-interface/satcom-modem-interface.ino
+++ satcom-modem-interface/satcom-modem-interface.ino
@@ -1,30 +1,45 @@
-#include <IridiumSBD.h>
+//#include <IridiumSBD.h>
+#include <Arduino.h>
 #include "wiring_private.h" // SERCOM pinPeripheral() function
 
-#define IridiumSerial Serial1
-#define DIAGNOSTICS false // Change this to see diagnostics
+//#define IridiumSerial Serial1
+//#define DIAGNOSTICS false // Change this to see diagnostics
+//IridiumSBD modem(IridiumSerial); // Declare the IridiumSBD object
 
-// Declare the IridiumSBD object
-IridiumSBD modem(IridiumSerial);
+#define RX_PIN 11
+#define TX_PIN 10
+#define RX_PAD SERCOM_RX_PAD_0
+#define TX_PAD UART_TX_PAD_2
 
-Uart Serial2 (&sercom1, 11, 10, SERCOM_RX_PAD_0, UART_TX_PAD_2);
-
-void SERCOM1_Handler()
+Uart RelaySerial (&sercom3, RX_PIN, TX_PIN, RX_PAD, TX_PAD);
+void SERCOM3_Handler() 
 {
-  Serial2.IrqHandler();
+  RelaySerial.IrqHandler();
 }
 
-uint32_t timer = millis();
+#define AWAKE_INTERVAL (60 * 1000)
+#define interruptPin 19
+
+#define LED_BLINK_TIMER 500
+
+uint32_t ledBlinkTimer = 2000000000L;
+
+volatile uint32_t awakeTimer = 0;
 
 void setup()
 {
-  while (!Serial);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // Show we're awake
+
   Serial.begin(115200);
-  Serial2.begin(115200);
+  RelaySerial.begin(115200);
+  
   // Assign pins 10 & 11 SERCOM functionality
-  pinPeripheral(10, PIO_SERCOM);
-  pinPeripheral(11, PIO_SERCOM);
-  IridiumSerial.begin(19200); // Start the serial port connected to the satellite modem
+  pinPeripheral(RX_PIN, PIO_SERCOM_ALT);
+  pinPeripheral(TX_PIN, PIO_SERCOM_ALT);
+
+  /*
+  //IridiumSerial.begin(19200); // Start the serial port connected to the satellite modem
 
   // Begin satellite modem operation
   Serial.println(F("Starting modem..."));
@@ -33,26 +48,97 @@ void setup()
     Serial.print(F("Begin failed: error "));
     Serial.println(result);
   }
+
+  // Get the IMEI
+  char IMEI[16];
+  result = modem.getIMEI(IMEI, sizeof(IMEI));
+  if (result != ISBD_SUCCESS)
+  {
+    Serial.print(F("getIMEI failed: error "));
+    Serial.println(result);
+    return;
+  }
+  Serial.print(F("IMEI is "));
+  Serial.print(IMEI);
+  Serial.println(F("."));
+
+  */
+
+  // Setup interrupt sleep pin
+  setupInterruptSleep();
 }
 
 void loop()
 {
-  if (millis() - timer > 2000) {
-    timer = millis();
-    // Get the IMEI
-    char IMEI[16];
-    int result = modem.getIMEI(IMEI, sizeof(IMEI));
-    if (result != ISBD_SUCCESS)
-    {
-      Serial.print(F("getIMEI failed: error "));
-      Serial.println(result);
-      Serial2.print(F("getIMEI failed: error "));
-      Serial2.println(result);
-    }
-    Serial.print(F("IMEI is "));
-    Serial.println(IMEI);
-    Serial2.print(F("IMEI is "));
-    Serial2.println(IMEI);
+  messageCheck();
+  sleepCheck();
+  checkLEDBlink();
+  
+}
+
+void messageCheck() {
+
+}
+
+void sleepCheck() {
+  if (nowTimeDiff(awakeTimer) > AWAKE_INTERVAL) {
+    // set pin mode to low
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("sleeping as timed out");
+    USBDevice.detach();
+    __WFI();  // wake from interrupt
+    USBDevice.attach();
+    delay(500);
+    Serial.println("wake due to interrupt");
+    Serial.println();
+    // request repeat of last message.
+    RelaySerial.println();
+    // toggle output of built-in LED pin
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+}
+
+void EIC_ISR(void) {
+  awakeTimer = millis(); // refresh awake timer.
+}
+
+void setupInterruptSleep() {
+  // whenever we get an interrupt, reset the awake clock.
+  attachInterrupt(digitalPinToInterrupt(interruptPin), EIC_ISR, CHANGE);
+  // Set external 32k oscillator to run when idle or sleep mode is chosen
+  SYSCTRL->XOSC32K.reg |=  (SYSCTRL_XOSC32K_RUNSTDBY | SYSCTRL_XOSC32K_ONDEMAND);
+  REG_GCLK_CLKCTRL  |= GCLK_CLKCTRL_ID(GCM_EIC) | // generic clock multiplexer id for the external interrupt controller
+                       GCLK_CLKCTRL_GEN_GCLK1 |   // generic clock 1 which is xosc32k
+                       GCLK_CLKCTRL_CLKEN;        // enable it
+  // Write protected, wait for sync
+  while (GCLK->STATUS.bit.SYNCBUSY);
+
+  // Set External Interrupt Controller to use channel 4
+  EIC->WAKEUP.reg |= EIC_WAKEUP_WAKEUPEN4;
+
+  PM->SLEEP.reg |= PM_SLEEP_IDLE_CPU;  // Enable Idle0 mode - sleep CPU clock only
+  //PM->SLEEP.reg |= PM_SLEEP_IDLE_AHB; // Idle1 - sleep CPU and AHB clocks
+  //PM->SLEEP.reg |= PM_SLEEP_IDLE_APB; // Idle2 - sleep CPU, AHB, and APB clocks
+
+  // It is either Idle mode or Standby mode, not both.
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;   // Enable Standby or "deep sleep" mode
+}
+
+unsigned long timeDiff(unsigned long x, unsigned long nowTime) {
+  if (nowTime >= x) {
+    return nowTime - x;
+  }
+  return (ULONG_MAX - x) + nowTime;
+}
+
+unsigned long nowTimeDiff(unsigned long x) {
+  return timeDiff(x, millis());
+}
+
+void checkLEDBlink() {
+  if (nowTimeDiff(ledBlinkTimer) > LED_BLINK_TIMER) {
+    ledBlinkTimer = millis(); // reset the timer
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   }
 }
 
