b7de51827b5e941dd67cb8bf0ae02f044b3ffac1
Mark Caudill <mark@mrkc.me>
2021-12-15 19:13:03 -0500

    Add SD card setup

--- satcom-modem-interface/satcom-modem-interface.ino
+++ satcom-modem-interface/satcom-modem-interface.ino
@@ -2,6 +2,13 @@
 #include <Arduino.h>
 #include "wiring_private.h" // SERCOM pinPeripheral() function
 
+// Ensure MISO/MOSI/SCK pins are not connected to the port replicator board
+#include <SPI.h>
+#include <SD.h>
+#define SDCardCSPin 4
+const String unsentMessagesDirectory = "messages/unsent";
+const String sentMessagesDirectory = "messages/sent";
+
 //#define IridiumSerial Serial1
 //#define DIAGNOSTICS false // Change this to see diagnostics
 //IridiumSBD modem(IridiumSerial); // Declare the IridiumSBD object
@@ -33,10 +40,28 @@ void setup()
 
   Serial.begin(115200);
   RelaySerial.begin(115200);
-  
+
   // Assign pins 10 & 11 SERCOM functionality
   pinPeripheral(RX_PIN, PIO_SERCOM_ALT);
   pinPeripheral(TX_PIN, PIO_SERCOM_ALT);
+
+  // Initialize SD card interface
+  Serial.println("Initializing SD card interface");
+  if (!SD.begin(SDCardCSPin)) {
+	  Serial.println("Error initializing SD card interface. Check card and wiring.");
+  }
+
+  // Setup SD card directories
+  if (!SD.exists(unsentMessagesDirectory)) {
+	  if (!SD.mkdir(unsentMessagesDirectory)) {
+		  Serial.println("Error creating directory: " + unsentMessagesDirectory);
+	  }
+  }
+  if (!SD.exists(sentMessagesDirectory)) {
+	  if (!SD.mkdir(sentMessagesDirectory)) {
+		  Serial.println("Error creating directory: " + sentMessagesDirectory);
+	  }
+  }
 
   /*
   //IridiumSerial.begin(19200); // Start the serial port connected to the satellite modem
