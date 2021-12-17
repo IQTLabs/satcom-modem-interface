8cdc730cb08e7c67bd9aa930bd7c965bc1fb2960
Robert Caudill <rcaudill@gmail.com>
2021-12-17 15:55:12 -0500

    remove test lines

--- satcom-modem-interface/satcom-modem-interface.ino
+++ satcom-modem-interface/satcom-modem-interface.ino
@@ -40,14 +40,6 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH); // Show we're awake
 
-  for (int i=0; i<3; i++) {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-  }
-  delay(2000);
-
   Serial.begin(115200);
   RelaySerial.begin(115200);
 
